5d2bf8cca0e6f7a53e8b668a205f5d245e9d3b48
ZeroTKA <zerotka@gmail.com>
2026-02-02 13:36:40 -0700

    Further Sounds And Coding
    Put a premature end to the cinematics. Perhaps continue later on but the show must go on.

--- Assets/_Project Specific Things/Script/Managers/Spawn_PoolManagerTest.cs
+++ Assets/_Project Specific Things/Script/Managers/Spawn_PoolManagerTest.cs
@@ -30,12 +30,24 @@ public class Spawn_PoolManagerTest : MonoBehaviour
     private IEnumerator ShowTime()
     {
         musicSource.PlayOneShot(music);
-        for (int i = 0; i < lightsList.Count; i++)
+        for (int i = 0; i < lightsList.Count - 1; i++)
         {
             yield return new WaitForSeconds(durationBeforeLights[i]);
             lightsList[i].SetActive(!lightsList[i].activeSelf);
             audioSources[i].PlayOneShot(audioClips[i]);
+
         }
+        yield return new WaitForSeconds(1.2f);
+        for (int i = 0; i < 2; i++)
+        {
+            audioSources[0].PlayOneShot(audioClips[6]); // loudspeaker warning siren
+            yield return new WaitForSeconds(.9f);
+            audioSources[0].PlayOneShot(audioClips[7]); // loudspeaker intruder alert
+            yield return new WaitForSeconds(2f);
+        }
+        yield return new WaitForSeconds(3.1f - 2f);
+        lightsList[5].SetActive(!lightsList[5].activeSelf);
+        audioSources[0].PlayOneShot(audioClips[5]);
         WaveManagerTest.instance.Wave1();
     }
 }
