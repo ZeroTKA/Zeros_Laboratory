4baf887f769809d564b2ddc5d2102b7a9560623b
ZeroTKA <zerotka@gmail.com>
2026-02-22 19:51:08 -0700

    PlayerMovementV2
    A good needs to feel good to move around. My first script works, but it's ass. Time to make it a lot lot better packed with more features.

---
+++ Assets/Scripts/PlayerMovementV2.cs
@@ -0,0 +1,134 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovementV2 : MonoBehaviour
+{
+    private enum MovementState { Grounded, Airborne, Crouching, Prone }
+    private MovementState currentState;
+
+    //-- Input Actions --//  -- To add an action, make sure to add in OnDisable, OnEnable, and in StartErrorChecking.
+    private InputAction lookAction;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private InputAction crouchAction;
+    private InputAction proneAction;
+
+    [Header("Character Setup")]
+    [SerializeField] private CharacterController controller; // Drag your character controller here
+    [SerializeField] private Camera playerCamera; // Drag your player camera here
+    [SerializeField] private Transform cameraPivotTransform; // Drag your camera pivot here
+
+    [Header("Movement Variables")]
+    [SerializeField] float currentMoveSpeed = 5f;
+    private Vector3 velocity; // used for gravity. Velocity.y persistant.
+    // -- Specialty Methods -- //
+
+    private void Awake()
+    {
+        StartErrorChecking();
+    }
+    void Update()
+    {
+        UpdateState();
+        HandleState();
+    }
+    private void OnEnable()
+    {
+        lookAction?.Enable();
+        moveAction?.Enable();
+        jumpAction?.Enable();
+        crouchAction?.Enable();
+        proneAction?.Enable();
+    }
+    private void OnDisable()
+    {
+        lookAction?.Disable();
+        moveAction?.Disable();
+        jumpAction?.Disable();
+        crouchAction?.Disable();
+        proneAction?.Disable();
+    }
+
+    // -- Main Methods -- //
+
+    private void HandleState()
+    {
+        switch(currentState)
+        {
+            case MovementState.Airborne:
+                HandleAirborne();
+                break;
+            case MovementState.Crouching:
+                HandleCrouching();
+                break;
+            case MovementState.Grounded:
+                HandleGrounded();
+                break;
+            case MovementState.Prone:
+                HandleProne();
+                break;
+        }
+    }
+    private void HandleAirborne()
+    {
+
+    }
+    private void HandleCrouching()
+    {
+
+    }
+    private void HandleGrounded()
+    {
+        if (moveAction == null) return;
+
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 inputMove = (transform.right * input.x + transform.forward * input.y) * currentMoveSpeed;
+        controller.Move((inputMove + velocity) * Time.deltaTime);
+    }
+    private void HandleProne()
+    {
+
+    }
+    private void UpdateState()
+    {
+        switch (currentState)
+        {
+            case MovementState.Grounded:
+                if (!controller.isGrounded)
+                    currentState = MovementState.Airborne;
+                break;
+            case MovementState.Airborne:
+                if (controller.isGrounded)
+                    currentState = MovementState.Grounded;
+                break;
+        }
+    }
+
+    // -- Supplemental Methods -- //
+    private void StartErrorChecking()
+    {
+        if (controller == null) Debug.LogError("[PlayerMovementV2] CharacterController not assigned.");
+        if (playerCamera == null) Debug.LogError("[PlayerMovementV2] playerCamera not assigned.");
+        if (cameraPivotTransform == null) Debug.LogError("[PlayerMovementV2] Camera Transform not assigned.");
+
+        if (TryGetComponent<PlayerInput>(out var playerInput))
+        {
+            moveAction = playerInput.actions["Move"];
+            lookAction = playerInput.actions["Look"];
+            jumpAction = playerInput.actions["Jump"];
+            crouchAction = playerInput.actions["Crouch"];
+            proneAction = playerInput.actions["Prone"];
+
+            if (moveAction == null) Debug.LogError("[PlayerMovementV2] Move action not found.");
+            if (lookAction == null) Debug.LogError("[PlayerMovementV2] Look action not found.");
+            if (jumpAction == null) Debug.LogError("[PlayerMovementV2] Jump action not found.");
+            if (crouchAction == null) Debug.LogError("[PlayerMovementV2] Crouch action not found.");
+            if (proneAction == null) Debug.LogError("[PlayerMovementV2] Prone action not found.");
+        }
+        else
+        {
+            Debug.LogError("[PlayerMovementV2] Unable to find PlayerInput.");
+        }
+    }
+}
