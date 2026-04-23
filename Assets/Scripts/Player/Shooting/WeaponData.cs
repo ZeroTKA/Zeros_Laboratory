82f99020c2a199c05495ad999157f2a09d316531
ZeroTKA <zerotka@gmail.com>
2026-04-22 21:09:14 -0600

    OnValidate()
    Added a bunch of checks for designers.

--- Assets/Scripts/Player/Shooting/WeaponData.cs
+++ Assets/Scripts/Player/Shooting/WeaponData.cs
@@ -13,8 +13,8 @@ public class WeaponData : ScriptableObject
     public enum FireModes
     {
         Semi = 1,
-        Auto = 2,
-        Burst = 4
+        Burst = 2,
+        Auto = 4
     }
 
     public enum ShootTypes
@@ -38,18 +38,28 @@ public class WeaponData : ScriptableObject
     [SerializeField] private int _clipSize;
     [SerializeField] private int _totalAmmoCapacity;
 
-    public FireModes AvailableFireModes => _availableFireModes;
     public ShootTypes ShootType => _shootType;
-    public float FireRate => _fireRate;
+    public GameObject ProjectilePrefab => _projectilePrefab;
+    public FireModes AvailableFireModes => _availableFireModes;
     public int BurstCount => _burstCount;
     public float BurstDelay => _burstDelay;
+    public float FireRate => _fireRate; 
     public float ReloadTime => _reloadTime;
     public int ClipSize => _clipSize;
     public int TotalAmmoCapacity => _totalAmmoCapacity;
-    public GameObject ProjectilePrefab => _projectilePrefab;
 
+
+#if UNITY_EDITOR
     private void OnValidate()
     {
-        
+        if (_shootType == ShootTypes.Projectile && _projectilePrefab == null) { Debug.LogWarning("[WeaponData] No prefab selected with projectile shooting. Select raycast or pick a prefab."); return; }
+        if (_availableFireModes == 0) { Debug.LogWarning("[WeaponData] Fire Mode can not be nothing. It must be something."); return; }
+        if (_availableFireModes.HasFlag(FireModes.Burst) && _burstCount < 2) { Debug.LogWarning("[WeaponData] Burst Count must be greater than 1 with fire mode Burst. If you want 1, pick Semi."); return; }
+        if (_availableFireModes.HasFlag(FireModes.Burst) && _burstDelay < 0) { Debug.LogWarning("[WeaponData] Burst Delay can not be a negative number with fire mode Burst."); return; }
+        if (_fireRate <= 0) { Debug.LogWarning("[WeaponData] Fire Rate must be greater than 0."); return; }
+        if (_reloadTime < 0) { Debug.LogWarning("[WeaponData] Reload Time can not be a negative number."); return; }
+        if (_clipSize <= 0) { Debug.LogWarning("[WeaponData] Clip Size must be greater than 0"); return; }
+        if (_totalAmmoCapacity < _clipSize) { Debug.LogWarning("[WeaponData] Total AmmoCapacity must be greater than Clip Size."); return; }
     }
+#endif
 }
