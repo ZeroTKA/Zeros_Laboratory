33193cac2db46c360673b857168cb8e39effb1ff
ZeroTKA <zerotka@gmail.com>
2026-06-30 19:02:20 -0600

    TestUp
    Simple Test. Now to somehow rig up some testing.

--- Assets/_Project Specific Things/SpawnManagerTest/Script/SceneDirector_SpawnManager.cs
+++ Assets/_Project Specific Things/SpawnManagerTest/Script/SceneDirector_SpawnManager.cs
@@ -1,13 +1,26 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class SceneDirector_SpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject enemeyPrefab;
-    [SerializeField] GameObject spawnPoint;
+    [SerializeField] GameObject spawnBurst;
+    [SerializeField] GameObject[] spawnBurstDual;
+    [SerializeField] GameObject spawn;
+    [SerializeField] GameObject[] spawnDual;
+    [SerializeField] GameObject spawnDuration;
+    [SerializeField] GameObject[] spawnDurationDual;
+
+    
 
     private void Start()
     {
-        StartCoroutine(SpawnManager.Instance.SpawnBurst(enemeyPrefab, 3, spawnPoint));
+        StartCoroutine(SpawnManager.Instance.SpawnBurst(enemeyPrefab, 100, spawnBurst));
+        StartCoroutine(SpawnManager.Instance.SpawnBurst(enemeyPrefab, 100, spawnBurstDual));
+
+        StartCoroutine(SpawnManager.Instance.Spawn(enemeyPrefab, 100, spawn, 15));
+        StartCoroutine(SpawnManager.Instance.Spawn(enemeyPrefab, 100, spawnDual, 15));
+
+        StartCoroutine(SpawnManager.Instance.SpawnByDuration(enemeyPrefab, 100, spawnDuration, .2f));
+        StartCoroutine(SpawnManager.Instance.SpawnByDuration(enemeyPrefab, 100, spawnDurationDual, .2f));
     }
 }
