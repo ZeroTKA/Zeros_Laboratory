e055028bf1298a95d99ecc9b6c3217a921666ba6
ZeroTKA <zerotka@gmail.com>
2026-07-24 11:59:23 -0600

    Works
    Made sure it works. Now the buttons need to do someth8ing.

--- Assets/Scripts/Managers/UIManager/PlayerMenuInput.cs
+++ Assets/Scripts/Managers/UIManager/PlayerMenuInput.cs
@@ -1,24 +1,49 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 
 public class PlayerMenuInput : MonoBehaviour
 {
-    //-- Input Actions --//  -- To add an action, make sure to add in OnDisable, OnEnable, and in StartErrorChecking.
+    // -- Unite Events -- //
+    public UnityEvent OnEscActionPress; // wire up to UI Manager.EscapeMenuButtonPressed
+
+
+    // -- Input Actions -- //  Make sure to add in Project Settings, OnDisable, OnEnable, and in StartErrorChecking.
     private InputAction escAction;
 
+    // -- Specialty Methods -- //
+    private void Awake()
+    {
+        StartErrorChecking();
+    }
+    private void Update()
+    {
+        if(escAction.WasPressedThisFrame())
+        {
+            OnEscActionPress?.Invoke();
+        }
+    }
+    private void OnDisable()
+    {
+        escAction?.Disable();
+    }
+    private void OnEnable()
+    {
+        escAction?.Enable();
+    }
+
+    // -- Supplemental Methods -- //
     private void StartErrorChecking()
     {
         if (TryGetComponent<PlayerInput>(out var playerInput))
         {
             escAction = playerInput.actions["Escape"];
             if (escAction == null) Debug.LogError("[PlayerMenuInput] Escape action not found.");
-
         }
         else
         {
             Debug.LogError("[PlayerMenuInput] Unable to find PlayerInput.");
         }
-
     }
 }
