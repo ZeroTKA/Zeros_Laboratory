cdedee78ca0e43b9ca84bea988bbe536bdf8226b
ZeroTKA <zerotka@gmail.com>
2026-07-29 11:25:04 -0600

    Changing To Pause Menu
    Changed UIManager to PauseMenu. Making UIManager a state machine to keep track of what state we are in.

---
+++ Assets/Scripts/Managers/UIManager/PauseMenu.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Escape Menu")]
+    [SerializeField] GameObject escMenu;
+
+    [SerializeField] Button quitButton;
+    [SerializeField] Button resumeButton;
+    [SerializeField] Button settingsButton;
+
+    [Header("Settings Menu")]
+    [SerializeField] GameObject settingsMenu;
+
+    [SerializeField] Button audioButton;
+    [SerializeField] Button backButton;
+    [SerializeField] Button displayButton;
+    [SerializeField] Button gameButton;
+    [SerializeField] Button graphicsButton;
+    [SerializeField] Button saveButton;
+    [SerializeField] Button discardButton;
+    [SerializeField] Button UIButton;
+    [SerializeField] Button AccessibilityButton;
+
+    [Header("Audio Menu")]
+    [SerializeField] GameObject audioMenu;
+
+    // -- Escape Menu Functions -- //
+    public void EscapeMenuButtonPressed()
+    {
+        escMenu.SetActive(!escMenu.activeSelf);
+        Cursor.lockState = escMenu.activeSelf ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+    public void ResumeWasClicked()
+    {
+        EscapeMenuButtonPressed();
+    }
+    public void SettingsWasClicked()
+    {
+        settingsMenu.SetActive(true);
+        escMenu.SetActive(false);
+    }
+    public void QuitWasClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif        
+    }
+}
--- Assets/Scripts/Managers/UIManager/UIManager.cs
+++ Assets/Scripts/Managers/UIManager/UIManager.cs
@@ -1,52 +1,6 @@
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    [Header("Escape Menu")]
-    [SerializeField] GameObject escMenu;
-
-    [SerializeField] Button quitButton;
-    [SerializeField] Button resumeButton;
-    [SerializeField] Button settingsButton;
-
-    [Header("Settings Menu")]
-    [SerializeField] GameObject settingsMenu;
-
-    [SerializeField] Button audioButton;
-    [SerializeField] Button backButton;
-    [SerializeField] Button displayButton;
-    [SerializeField] Button gameButton;
-    [SerializeField] Button graphicsButton;
-    [SerializeField] Button saveButton;
-    [SerializeField] Button discardButton;
-
-    [Header("Audio Menu")]
-    [SerializeField] GameObject audioMenu;
-
-    // -- Escape Menu Functions -- //
-    public void EscapeMenuButtonPressed()
-    {
-        escMenu.SetActive(!escMenu.activeSelf);
-        Cursor.lockState = escMenu.activeSelf ? CursorLockMode.Confined : CursorLockMode.None;
-    }
-    public void ResumeWasClicked()
-    {
-        Debug.Log("Resume Clicked");
-        EscapeMenuButtonPressed();
-    }
-    public void SettingsWasClicked()
-    {
-        Debug.Log("Settings Clicked");
-    }
-    public void QuitWasClicked()
-    {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-    Application.Quit();
-#endif
-        Debug.Log("Quit Clicked");
-    }
+    public enum MenuState { Pause, Inventory, Dialogue, Gameplay }
 }
